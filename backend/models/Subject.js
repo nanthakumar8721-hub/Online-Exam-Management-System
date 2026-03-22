93c80c5067e7a71ca04bb5b8a9ef69205fb71a62
NANTHA KUMAR <nanthakumar8721@gmail.com>
2026-03-22 12:25:10 +0530

    Initial commit

--- backend/models/Subject.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SubjectSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true, unique: true },
-  description: String
-});
-
-module.exports = mongoose.model('Subject', SubjectSchema);
